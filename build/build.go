546ea38f5706f7409579db76117ca4014b64aa5d
Markus Herpich <markus@endocode.com>
2019-02-15 14:28:24 +0100

    Implement helper functions for adding nodes in build phase
    Add functions to send filenodes to the server in the build phase.
    Those will be used e.g. by the qmstrctl command after the actual
    build went through.

---
+++ build/build.go
@@ -0,0 +1,38 @@
+package build
+
+import (
+	"context"
+
+	"github.com/QMSTR/go-qmstr/service"
+)
+
+// QmstrBuildClient is used to send build information to the QMSTR master server
+type QmstrBuildClient struct {
+	buildService service.BuildServiceClient
+}
+
+// BuildFiles sends the given FileNode instances to the QMSTR master server
+func (qbc *QmstrBuildClient) BuildFiles(fileNodes ...*service.FileNode) error {
+	build, err := qbc.buildService.Build(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, fn := range fileNodes {
+		build.Send(fn)
+	}
+	return nil
+}
+
+// PackageFiles adds the given FileNode instances to the current package
+func (qbc *QmstrBuildClient) PackageFiles(fileNodes ...*service.FileNode) error {
+	pkg, err := qbc.buildService.Package(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, fn := range fileNodes {
+		pkg.Send(fn)
+	}
+	return nil
+}
